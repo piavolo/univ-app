374bf6a600de7f2f0b9b4666bc301bcd048b30e6
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-05-01 22:01:32 +0200

    Add bcrypt gem and secure password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_29_184058) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_194711) do
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_29_184058) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

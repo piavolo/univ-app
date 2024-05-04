1b994a7cf302e8ddcab12521767c6612ff6bfb03
Pablo Gandarillas <XV.I.MCMLXXXIX@gmail.com>
2024-05-04 16:56:32 +0200

    Add students and courses many-to-many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_194711) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_143708) do
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
